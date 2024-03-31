200ee3bb3ed49755cc696f62bcf0c60eb8d70595
winstencoellins <winstencoellinss@gmail.com>
2024-03-31 13:46:20 +0700

    Change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Barlow } from "next/font/google";
 import "./globals.css";
 
-const inter = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700", "800", "900"] });
+const inter = Barlow({ subsets: ["latin"], weight: ["400", "500", "600", "700", "800", "900"] });
 
 export const metadata: Metadata = {
   title: "Split Bill Application",
